9cd95c34649c286caa72f24ae793ade0cc72173e
gauravv69 <gauravkadam2422@gmail.com>
2025-07-28 15:48:29 +0530

    Email Part done!!

--- assets/js/main.js
+++ assets/js/main.js
@@ -37,3 +37,32 @@ const shadowHeader = () =>{
                         : header.classList.remove('shadow-header')
 }
 window.addEventListener('scroll', shadowHeader)
+
+/*=============== Email JS ===============*/
+const contactForm = document.getElementById('contact-form'),
+    contactMessage = document.getElementById('contact-message')
+
+const sendEmail = (e) =>{
+    e.preventDefault()
+
+    // serviceID - templateID - #form - publicKey
+    emailjs.sendForm('service_q4se4gg','template_a7s4106','#contact-form','F88f1PGdbLSpPtjmp')
+    .then(() => {
+        // Show sent message
+        contactMessage.textContent = 'Message sent successfully! ✅'
+
+        // Remove message after five seconds
+        setTimeout(() =>{
+            contactMessage.textContent = ''
+        }, 5000)
+
+        // Clear input fields
+        contactForm.reset()
+
+    }, () =>{
+        // Show error message
+        contactMessage.textContent = 'Message not sent (service error) ❌'
+    })
+}
+
+contactForm.addEventListener('submit', sendEmail)
